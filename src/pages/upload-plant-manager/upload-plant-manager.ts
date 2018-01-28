f543c3af7c3318332c8b477f6f3b311107e66baf
zaq962 <568359227@qq.com>
2018-01-28 14:48:48 +0800

    上传管理模拟数据联调

--- src/pages/upload-plant-manager/upload-plant-manager.ts
+++ src/pages/upload-plant-manager/upload-plant-manager.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { File } from '@ionic-native/file';
+import { TyNetworkServiceProvider } from '../../providers/ty-network-service/ty-network-service';
+import { AppServiceProvider, AppGlobal } from '../../providers/app-service/app-service';
+import { BasePage } from '../base/base';
 /**
  * Generated class for the UploadPlantManagerPage page.
  *
@@ -13,7 +16,7 @@ import { File } from '@ionic-native/file';
   selector: 'page-upload-plant-manager',
   templateUrl: 'upload-plant-manager.html',
 })
-export class UploadPlantManagerPage {
+export class UploadPlantManagerPage extends BasePage {
   waitLoadArray: Array<any> = [{picUrl:"assets/imgs/test.png",no:"zj50003",picNum:"8"},{picUrl:"assets/imgs/test.png",no:"zj50003",picNum:"8"},{picUrl:"assets/imgs/test.png",no:"zj50003",picNum:"8"},{picUrl:"assets/imgs/test.png",no:"zj50003",picNum:"8"},{picUrl:"assets/imgs/test.png",no:"zj50003",picNum:"8"},{picUrl:"assets/imgs/test.png",no:"zj50003",picNum:"8"},{picUrl:"assets/imgs/test.png",no:"zj50003",picNum:"8"}];
   hadUploadArray:Array<any> = [{picUrl:"assets/imgs/test.png",no:"zj50003",picNum:"8"},{picUrl:"assets/imgs/test.png",no:"zj50003",picNum:"3"}];
   reviewArray:Array<any> = [{picUrl:"assets/imgs/test.png",no:"zj50003",picNum:"8"},{picUrl:"assets/imgs/test.png",no:"zj50003",picNum:"9"},{picUrl:"assets/imgs/test.png",no:"zj50003",picNum:"4"}];
@@ -23,15 +26,20 @@ export class UploadPlantManagerPage {
   total1 = -1;
   currentPage2: number = 1;
   pageSize2: number = 20;
+  total2 = -1;
   currentPage3: number = 1;
   pageSize3: number = 20;
+  total3 = -1;
   type:String = "hadUpload";//待上传waitLoad 已上传hadUpload 审核中review
-  constructor(private file: File,public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public toastCtrl: ToastController, ,private file: File,public navCtrl: NavController, public navParams: NavParams,private net:TyNetworkServiceProvider) {
+    super(navCtrl, navParams, toastCtrl);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad UploadPlantManagerPage');
     this.readLocalFiles();
+    this.sendQueryPlantsRequest2(1,null,false);
+    this.sendQueryPlantsRequest3(1,null,false);
   }
   uploadBtnCliked(){
     console.log('批量上传');
@@ -93,29 +101,67 @@ export class UploadPlantManagerPage {
   doRefresh2(refresher) {
     //刷新
     console.log("下拉刷新");
-    // this.sendPayHistoryRequest(1, refresher);
+    this.sendQueryPlantsRequest2(1, refresher);
     // if (refresher != null) {
     //   refresher.complete();
     // }
   }
   doInfinite2(refresher) {
     console.log("上拉加载更多");
-    // this.sendPayHistoryRequest(this.currentPage+1, refresher);
+    this.sendQueryPlantsRequest2(this.currentPage2+1, refresher);
     // if (refresher != null) {
     //   refresher.complete();
     // }
   }
+
+  sendQueryPlantsRequest2(page: any, refresher: any,isLoading?=true) {
+    let params =
+      {
+        "userName":AppServiceProvider.getInstance().userinfo.loginData.userName,
+        "start":  page==1?0:this.reviewArray.length,
+        "length": this.reviewArray,
+      };
+    this.net.httpPost(AppGlobal.API.recordList, params, msg => {
+      let obj = JSON.parse(msg);
+      if (obj.ret == AppGlobal.RETURNCODE.succeed) {
+        if (page == 1) {
+          this.reviewArray = [];
+        }
+        let list = obj.data;
+        this.total2 = obj.recordsTotal;
+        for (let index = 0; index < list.length; index++) {
+          let element = list[index];
+          this.reviewArray.push(element);
+        }
+        this.currentPage2 = page;
+        if (refresher != null) {
+          refresher.complete();
+        }
+      } else {
+        this.toast(obj.ACTION_RETURN_MESSAGE);
+      }
+      this.total2 = 0;
+    }, error => {
+      this.toast(error);
+      this.total2 = 0;
+      if (refresher != null) {
+        refresher.complete();
+      }
+    }, isLoading);
+  }
+
+  
   doRefresh3(refresher) {
     //刷新
     console.log("下拉刷新");
-    // this.sendPayHistoryRequest(1, refresher);
+    this.sendQueryPlantsRequest3(1, refresher);
     // if (refresher != null) {
     //   refresher.complete();
     // }
   }
   doInfinite3(refresher) {
     console.log("上拉加载更多");
-    // this.sendPayHistoryRequest(this.currentPage+1, refresher);
+    this.sendQueryPlantsRequest3(this.currentPage3+1, refresher);
     // if (refresher != null) {
     //   refresher.complete();
     // }
@@ -123,6 +169,41 @@ export class UploadPlantManagerPage {
 
 
   
+  sendQueryPlantsRequest3(page: any, refresher: any,isloading?=true) {
+    let params =
+      {
+        "userName":AppServiceProvider.getInstance().userinfo.loginData.userName,
+        "start":  page==1?0:this.hadUploadArray.length,
+        "length": this.hadUploadArray,
+      };
+    this.net.httpPost(AppGlobal.API.recordList, params, msg => {
+      let obj = JSON.parse(msg);
+      if (obj.ret == AppGlobal.RETURNCODE.succeed) {
+        if (page == 1) {
+          this.hadUploadArray = [];
+        }
+        let list = obj.data;
+        this.total3 = obj.recordsTotal;
+        for (let index = 0; index < list.length; index++) {
+          let element = list[index];
+          this.hadUploadArray.push(element);
+        }
+        this.currentPage3 = page;
+        if (refresher != null) {
+          refresher.complete();
+        }
+      } else {
+        this.toast(obj.ACTION_RETURN_MESSAGE);
+      }
+      this.total3 = 0;
+    }, error => {
+      this.toast(error);
+     this.total3 = 0;
+      if (refresher != null) {
+        refresher.complete();
+      }
+    }, isloading);
+  }
 
 
 
